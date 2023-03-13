231aa7fe68b9447e6c8d3508a20f1b18c45e528c
l3g0craft <96246326+l3g0craft@users.noreply.github.com>
2023-03-13 16:27:49 +0100

    Update index.mk

--- index.mk
+++ index.mk
@@ -1,4 +1,10 @@
-# installation
+# resumer
+c'est page webb va renplacer l'index de ngninx pour la transformer en ruche
 
-1.  requis:
-   -  nginx
+# installation avec nginx
+
+1.  aller dans /var/www/html
+2.  supprimer index.html 
+2.  faire un "git clone https://github.com/l3g0craft/ruche "pour telecharger le projet
+4.  voila c'est fini vous avez une belle ruche 
+
